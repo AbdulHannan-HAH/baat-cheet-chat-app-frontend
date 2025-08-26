dd0672f70d0e47d02b8839510b34925e5ed854e2
AbdulHannan-HAH <2023cs802@student.uet.edu.pk>
2025-08-25 22:47:15 -0700

    deploymnet

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://baatcheet-backend-s5cs.onrender.com/api",
   withCredentials: true,
 });
+
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
